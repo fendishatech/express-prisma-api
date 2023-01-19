eddacc5435f1f37f08ebc8bc33241ef901c3d1b2
liyuKnow <kidus2011@gmail.com>
2023-01-18 17:39:26 -0800

    user full crud

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -6,4 +6,82 @@ export const userController = {
     const users = await prisma.user.findMany();
     return res.json(users);
   },
+  async store(req: Request, res: Response) {
+    const userData = req.body;
+
+    const user = await prisma.user.create({
+      data: {
+        fName: userData.fName,
+        lName: userData.lName,
+      },
+    });
+
+    return res.json({
+      success: true,
+      payload: user,
+      message: "User was created successfully",
+    });
+  },
+  async getUser(req: Request, res: Response) {
+    const paramId = req.params.id;
+    const user = await prisma.user.findUnique({
+      where: {
+        id: paramId,
+      },
+    });
+    return res.json(user);
+  },
+  async search(req: Request, res: Response) {
+    const q = req.params.query;
+    const users = await prisma.user.findMany({
+      where: {
+        fName: {
+          startsWith: q,
+        },
+        // OR: {
+        //   fName: {
+        //     startsWith: q,
+        //   },
+        //   lName: {
+        //     startsWith: q,
+        //   },
+        // },
+      },
+    });
+    return res.json(users);
+  },
+  async updateUser(req: Request, res: Response) {
+    const paramId = req.params.id;
+    const userData = req.body;
+    const updatedUser = await prisma.user.update({
+      where: {
+        id: paramId,
+      },
+      data: {
+        ...req.body,
+      },
+    });
+    return res.json({
+      message: {
+        type: "success",
+        content: "User was updated successfully",
+      },
+      updatedUser,
+    });
+  },
+  async deleteUser(req: Request, res: Response) {
+    const paramId = req.params.id;
+    const deletedUser = await prisma.user.delete({
+      where: {
+        id: paramId,
+      },
+    });
+    return res.json({
+      message: {
+        type: "success",
+        content: "User was deleted successfully",
+      },
+      deletedUser,
+    });
+  },
 };
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -3,5 +3,10 @@ import { userController } from "../controllers/user.controller";
 const routes = Router();
 
 routes.get("/", userController.index);
+routes.get("/:id", userController.getUser);
+routes.get("/search/:query", userController.search);
+routes.post("/", userController.store);
+routes.put("/:id", userController.updateUser);
+routes.delete("/:id", userController.deleteUser);
 
 export default routes;
